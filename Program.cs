48d0ae73bae25a6296832dc4cf27659aef39ea5f
caio-jordan <caio.jordan15@hotmail.com>
2025-06-14 21:04:30 -0300

    Update Reserva and program

--- Program.cs
+++ Program.cs
@@ -6,14 +6,17 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
-
+Pessoa p1 = new Pessoa(nome: "Paulina");
+Pessoa p2 = new Pessoa(nome: "Paulo");
+Pessoa p3 = new Pessoa(nome: "Predo");
+Pessoa p4 = new Pessoa(nome: "Petra");
 hospedes.Add(p1);
 hospedes.Add(p2);
+hospedes.Add(p3);
+hospedes.Add(p4);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Standard", capacidade: 5, valorDiaria: 299.99M);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
 Reserva reserva = new Reserva(diasReservados: 5);
@@ -23,3 +26,5 @@ reserva.CadastrarHospedes(hospedes);
 // Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
 Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+// Exibe os detalhes da reserva
+Console.WriteLine($"Detalhes da reserva: {reserva.Suite.TipoSuite}, Capacidade: {reserva.Suite.Capacidade}, Valor Diária: {reserva.Suite.ValorDiaria:C}");
